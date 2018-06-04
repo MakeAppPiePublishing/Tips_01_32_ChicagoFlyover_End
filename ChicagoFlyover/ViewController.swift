947acbbce0fe183d8fd66a9273be457793137387
Steve Lipton <stevelipton@brk-16892.local>
2018-06-04 07:17:51 -0500

    End State Initial Commit

--- ChicagoFlyover/ViewController.swift
+++ ChicagoFlyover/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController:UIViewController{
 
+    @IBOutlet weak var mapview: MKMapView!
+    var timer = Timer()
+    let startlocation = CLLocationCoordinate2DMake(41.851, -87.6238) //23rd and S. Michigan
+    let endLatitude = 41.91 //North Avenue Beach
+    
+    @IBAction func flyOverChicago(_ sender: UIButton) {
+        animate()
+    }
+    
+    func startMapView(){
+        mapview.frame = view.frame
+        let mapcamera = MKMapCamera(lookingAtCenter: startlocation, fromDistance: 500, pitch: 60, heading: 0.0)
+        mapview.mapType = .standard
+        mapview.camera = mapcamera
+    }
+    
+    func animate(){
+        var currentLatitude = startlocation.latitude
+        let increment = 0.00005
+        let _ = Timer.scheduledTimer(withTimeInterval: (1.0/30.0), repeats: true) { (timer) in
+            currentLatitude += increment
+            if currentLatitude >= self.endLatitude {
+                timer.invalidate()
+            }
+            self.mapview.camera.centerCoordinate.latitude = currentLatitude
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startMapView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+
+
+
+
+
+
 
